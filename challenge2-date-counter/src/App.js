172c335144e4a28a8e25af2ba2a03e54ae9d0aa8
wakabibrian <wakabibrian95@gmail.com>
2023-07-03 22:34:00 +0300

    Started a new project

---
+++ challenge2-date-counter/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+    return (
+        <div className="App">
+            <Counter />
+        </div>
+    );
+}
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [step, setStep] = useState(0);
+    const date = new Date("June 21 2027");
+
+    date.setDate(date.getDate() + count);
+
+    return (
+        <>
+            <div>
+                <button onClick={() => setStep((s) => s - 1)}>-</button>
+                <span>Step: {step}</span>
+                <button onClick={() => setStep((s) => s + 1)}>+</button>
+            </div>
+            <div>
+                <button onClick={() => setCount((c) => c - step)}>-</button>
+                <span>Count: {count}</span>
+                <button onClick={() => setCount((c) => c + step)}>+</button>
+            </div>
+            <p>{`Today is ${date.toDateString()}`}</p>
+        </>
+    );
+}
