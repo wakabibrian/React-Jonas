76424dd7cfe28cbfdef80ff3ec3a93df3aab2fa1
wakabibrian <wakabibrian95@gmail.com>
2023-07-29 04:57:04 +0300

    Finished ternary operator

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -74,18 +74,23 @@ function Header() {
 
 function Menu() {
     const pizzas = pizzaData;
+    // const pizzas = [];
     const numPizzas = pizzas.length;
 
     return (
         <main className="menu">
             <h2>Our Menu</h2>
 
-            {numPizzas > 0 && (
+            {numPizzas > 0 ? (
                 <ul className="pizzas">
                     {pizzas.map((pizza) => (
                         <Pizza pizzaObj={pizza} key={pizza.name} />
                     ))}
                 </ul>
+            ) : (
+                <p>
+                    We're still working on our menu. Please come back later :)
+                </p>
             )}
 
             {/* <Pizza
@@ -120,7 +125,7 @@ function Pizza(props) {
 
 function Footer() {
     const hour = new Date().getHours();
-    const openHour = 3;
+    const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
     console.log(isOpen);
@@ -130,7 +135,7 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {isOpen && (
+            {isOpen ? (
                 <div className="order">
                     <p>
                         We're open until {closeHour}:00. Come visit us or Order
@@ -138,6 +143,11 @@ function Footer() {
                     </p>
                     <button className="btn">Order</button>
                 </div>
+            ) : (
+                <p>
+                    We're happy to welcome you between {openHour}:00 and{" "}
+                    {closeHour}:00.
+                </p>
             )}
         </footer>
     );
