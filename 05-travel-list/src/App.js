ea853ed2534326cde5802c728e2c3a20a1a6b170
wakabibrian <wakabibrian95@gmail.com>
2023-08-09 06:11:23 +0300

    Finished clear list

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -19,6 +19,14 @@ export default function App() {
         );
     }
 
+    function handleClearList() {
+        const confirmed = window.confirm(
+            "Are you sure you want to delete all items?"
+        );
+
+        if (confirmed) setItems([]);
+    }
+
     return (
         <div className="app">
             <Logo />
@@ -27,6 +35,7 @@ export default function App() {
                 items={items}
                 onDeleteItem={handleDeleteItem}
                 onToggleItems={handleToggleItem}
+                onClearList={handleClearList}
             />
             <Stats items={items} />
         </div>
@@ -83,7 +92,7 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItems }) {
+function PackingList({ items, onDeleteItem, onToggleItems, onClearList }) {
     const [sortBy, setSortBy] = useState("input");
 
     let sortedItems;
@@ -122,6 +131,7 @@ function PackingList({ items, onDeleteItem, onToggleItems }) {
                     <option value="description">Sort by description</option>
                     <option value="packed">Sort by packed status</option>
                 </select>
+                <button onClick={onClearList}>Clear List</button>
             </div>
         </div>
     );
